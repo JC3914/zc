7e63a9f716c72a4817d4ce8dfcef4ca172d5bdf2
ljc <2892394011@qq.com>
2020-12-31 19:27:05 +0800

    1.0

---
+++ js/a.js
@@ -0,0 +1,5 @@
+console.log("a.js");
+img.onclick=function(){
+    // js里面的路径怎么写？
+    img.src="../images/6.jpg"
+}
---
+++ js/index.js
@@ -0,0 +1,16 @@
+var a=()=>{
+    console.log("index.js里面的a方法");
+}
+var b=()=>{
+    console.log("index.js里面的b方法");
+}
+var sum=function(){
+    var num1=10;
+    var num2=20;
+    console.log(num1+num2);
+}
+var obj={
+    c:1,
+    d:2
+}
+let {c,d}=obj;
